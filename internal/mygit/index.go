0ac8fffd224aec82b43af8574792ce7e2420dfcd
Richard Jennings <richardjennings@gmail.com>
2024-01-03 16:53:08 +0000

    os specific fileinfo handling

--- internal/mygit/index.go
+++ internal/mygit/index.go
@@ -8,7 +8,9 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"runtime"
 	"sort"
+	"syscall"
 	"time"
 )
 
@@ -104,6 +106,40 @@ func (idx *index) addWdFile(f *wdFile) error {
 	}
 
 	return nil
+}
+
+func (wdf *wdFile) toIndexItem() (*indexItem, error) {
+	if wdf.sha == nil {
+		return nil, errors.New("missing sha from working directory file toIndexItem")
+	}
+	item := &indexItem{indexItemP: &indexItemP{}}
+	switch runtime.GOOS {
+	case "darwin":
+	case "linux":
+	default:
+		return nil, errors.New("setItemOsSpecificStat not implemented, unsupported OS")
+	}
+	setItemOsSpecificStat(wdf.finfo, item)
+	item.Dev = uint32(wdf.finfo.Sys().(*syscall.Stat_t).Dev)
+	item.Ino = uint32(wdf.finfo.Sys().(*syscall.Stat_t).Ino)
+	if wdf.finfo.IsDir() {
+		item.Mode = uint32(040000)
+	} else {
+		item.Mode = uint32(0100644)
+	}
+	item.Uid = wdf.finfo.Sys().(*syscall.Stat_t).Uid
+	item.Gid = wdf.finfo.Sys().(*syscall.Stat_t).Gid
+	item.Size = uint32(wdf.finfo.Size())
+	copy(item.Sha[:], wdf.sha)
+	nameLen := len(wdf.path)
+	if nameLen < 0xFFF {
+		item.Flags = uint16(len(wdf.path))
+	} else {
+		item.Flags = 0xFFF
+	}
+	item.Name = []byte(wdf.path)
+
+	return item, nil
 }
 
 func (m *MyGit) newIndex() *index {
---
+++ internal/mygit/mygit_darwin.go
@@ -0,0 +1,15 @@
+//go:build darwin
+
+package mygit
+
+import (
+	"os"
+	"syscall"
+)
+
+func setItemOsSpecificStat(info os.FileInfo, item *indexItem) {
+	item.CTimeS = uint32(info.Sys().(*syscall.Stat_t).Ctimespec.Sec)
+	item.CTimeN = uint32(info.Sys().(*syscall.Stat_t).Ctimespec.Nsec)
+	item.MTimeS = uint32(info.Sys().(*syscall.Stat_t).Mtimespec.Sec)
+	item.MTimeN = uint32(info.Sys().(*syscall.Stat_t).Mtimespec.Nsec)
+}
