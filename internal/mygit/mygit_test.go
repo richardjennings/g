a6eb0d62a9efbb97fc444bd6cfd3f75049a80cb4
Richard Jennings <richardjennings@gmail.com>
2024-01-09 21:44:17 +0000

    tidy up tests a bit

--- internal/mygit/mygit_test.go
+++ internal/mygit/mygit_test.go
@@ -43,6 +43,7 @@ func Test_AddFile_Status_Commit(t *testing.T) {
 	dir := testDir(t)
 	defer func() { _ = os.RemoveAll(dir) }()
 	testConfigure(t, dir)
+
 	if err := Init(); err != nil {
 		t.Fatal(err)
 	}
@@ -53,65 +54,37 @@ func Test_AddFile_Status_Commit(t *testing.T) {
 	}
 
 	// status should have an object
-	buf := bytes.NewBuffer(nil)
-	if err := Status(buf); err != nil {
-		t.Fatal(err)
-	}
-	assert.Equal(t, " ?? hello\n", buf.String())
+	testStatus(t, " ?? hello\n")
 
 	// add the file to the index
-	if err := Add("."); err != nil {
-		t.Fatal(err)
-	}
+	testAdd(t, ".", 1)
 	files := testListFiles(t, config.ObjectPath(), false)
 	assert.Equal(t, 1, len(files))
 
 	// status should be added
-	buf = bytes.NewBuffer(nil)
-	if err := Status(buf); err != nil {
-		t.Fatal(err)
-	}
-	assert.Equal(t, "A  hello\n", buf.String())
+	testStatus(t, "A  hello\n")
 
 	// create commit
-	sha, err := Commit()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(sha) != 20 {
-		t.Errorf("expected sha len 20 got %d", len(sha))
-	}
+	testCommit(t)
+
 	files = testListFiles(t, config.ObjectPath(), false)
 	// blob, tree object, commit object
 	assert.Equal(t, 3, len(files))
 
 	// Test adding a modified file to the index
-
 	// update a file
 	if err := os.WriteFile(filepath.Join(dir, "hello"), []byte("hello world"), 0644); err != nil {
 		t.Fatal(err)
 	}
 	// status should be modified
-	buf = bytes.NewBuffer(nil)
-	if err := Status(buf); err != nil {
-		t.Fatal(err)
-	}
-	assert.Equal(t, " M hello\n", buf.String())
+	testStatus(t, " M hello\n")
 	// add the file to the index
-	if err := Add("."); err != nil {
-		t.Fatal(err)
-	}
-	// create commit
-	sha, err = Commit()
-	if err != nil {
-		t.Fatal(err)
-	}
+	testAdd(t, ".", 1)
+
+	testCommit(t)
+
 	// status should be empty
-	buf = bytes.NewBuffer(nil)
-	if err := Status(buf); err != nil {
-		t.Fatal(err)
-	}
-	assert.Equal(t, "", buf.String())
+	testStatus(t, "")
 }
 
 func testDir(t *testing.T) string {
@@ -147,4 +120,34 @@ func testListFiles(t *testing.T, path string, dirs bool) []string {
 		t.Fatal(err)
 	}
 	return files
+}
+
+func testAdd(t *testing.T, path string, numIdxFiles int) {
+	if err := Add(path); err != nil {
+		t.Fatal(err)
+	}
+	files, err := LsFiles()
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Len(t, files, numIdxFiles)
+}
+
+func testStatus(t *testing.T, expected string) {
+	buf := bytes.NewBuffer(nil)
+	if err := Status(buf); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, expected, buf.String())
+}
+
+func testCommit(t *testing.T) []byte {
+	sha, err := Commit()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(sha) != 20 {
+		t.Errorf("expected sha len 20 got %d", len(sha))
+	}
+	return sha
 }
